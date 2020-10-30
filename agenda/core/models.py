e23f5c3cbf54af0cb4743e86864fb01b12b5204d
dlucas71 <deciolucas@hotmail.com>
2020-10-30 15:45:20 -0300

    Alteracao do arquivo models,py

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 class Evento(models.Model):
-    titulo = models.CharField(max_length=100),
-    descricao = models.TextField(blank=True, null=True),
-    local_evento = models.TextField(blank=True, null=True),
-    data_evento = models.DateTimeField(),
-    data_criacao = models.DateTimeField(auto_now=True),
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    local_evento = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField()
+    data_criacao = models.DateTimeField(auto_now=True)
+
+    #class Meta:
+     #   db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
